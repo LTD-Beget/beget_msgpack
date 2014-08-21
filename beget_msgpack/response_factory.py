934109ef4664fcef39586c0d62a39fdfd9c02c83
Constantin Conovaloff <constantin@conovaloff.com>
2014-08-21 18:26:25 +0400

    add error if answer not packed in msgpack or get error from fcgi

--- beget_msgpack/response_factory.py
+++ beget_msgpack/response_factory.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from .lib.response import Response
 import msgpack
-from .lib.logger import Logger
 
+from .lib.logger import Logger
+from .lib.response import Response
+from .lib.errors.error_constructor import ErrorConstructor
 
 class ResponseFactory:
     """
@@ -20,18 +21,35 @@ class ResponseFactory:
         pass
 
     def get_response_by_msgpack_answer(self, answer):
-        self.logger.debug('ResponseFactory->by_msgpack: get answer: %s', answer)
+        self.logger.debug('ResponseFactory:by_msgpack: get answer: %s', answer)
         return Response(answer)
 
     def get_response_by_fcgi_answer(self, answer):
-        self.logger.debug('ResponseFactory->by_fcgi: get answer: %s', repr(answer))
+        self.logger.debug('ResponseFactory:by_fcgi: get answer: %s', repr(answer))
         code, header, raw_answer, error = answer
-        answer_unpack = msgpack.unpackb(raw_answer)
-        self.logger.debug('ResponseFactory->by_fcgi: change it to: %s', repr(answer_unpack))
+
+        # Если fcgi клиент сообщил нам об ошибке
+        if error:
+            self.logger.error('ResponseFactory:by_fcgi: get error in answer: %s' % error)
+            return self.get_response_by_request_error(ErrorConstructor.TYPE_ERROR_UNKNOWN,
+                                                      error,
+                                                      ErrorConstructor.CODE_ERROR_UNKNOWN)
+
+        try:
+            answer_unpack = msgpack.unpackb(raw_answer)
+
+        # Если ответ из сети пришел не запакованный в msgpack, то это ошибка
+        except msgpack.exceptions.ExtraData:
+            self.logger.error('ResponseFactory->by_fcgi: msgpack could not unpack answer')
+            return self.get_response_by_request_error(ErrorConstructor.TYPE_ERROR_UNKNOWN,
+                                                      answer,
+                                                      ErrorConstructor.CODE_ERROR_UNKNOWN)
+
+        self.logger.debug('ResponseFactory:by_fcgi: change it to: %s', repr(answer_unpack))
         return Response(answer_unpack)
 
     def get_response_by_request_error(self, type_error=None, message=None, code=None):
-        self.logger.debug('ResponseFactory->by_request_error: get code: %s, description: %s',
+        self.logger.debug('ResponseFactory:by_request_error: get code: %s, description: %s',
                           repr(code),
                           repr(message))
         response = Response()
