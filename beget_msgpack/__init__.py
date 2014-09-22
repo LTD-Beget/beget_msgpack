40b24493d8092dc49e7b636bb87a6620db333be5
Constantin Conovaloff <constantin@conovaloff.com>
2014-09-22 16:07:30 +0400

    added the ability to get transport

--- beget_msgpack/__init__.py
+++ beget_msgpack/__init__.py
@@ -4,5 +4,10 @@ from .server import Server
 from .controller import Controller
 from .handler import Handler
 from .lib.logger import Logger
+from .transport import Transport
 
 from ._version import __version__
+
+
+def get_transport(config):
+    return Transport(config)
