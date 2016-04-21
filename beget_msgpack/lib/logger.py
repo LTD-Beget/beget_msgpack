b05dd14ec13dfacd78e6830745a0f1b23b62a6c4
elozin <elozin@beget.ru>
2016-04-21 12:47:29 +0300

    Внедрение новой системы сквозных логов

--- beget_msgpack/lib/logger.py
+++ beget_msgpack/lib/logger.py
@@ -5,17 +5,41 @@ import uuid
 
 class LoggerAdapterRequestId(logging.LoggerAdapter):
 
+    static_global_request_id = None
+    static_platform = 'pyportal'
+    static_handler = 'msgpack'
+
+    def __init__(self, logger, extra):
+        super(LoggerAdapterRequestId, self).__init__(logger, extra)
+        self.extra['platform'] = self.static_platform
+        self.extra['handler'] = self.static_handler
+
     def request_id_generate(self):
-        self.extra = {'request_id': str(uuid.uuid4())[:8]}
+        self.logger.request_id = str(uuid.uuid4())[:8]
 
     def request_id_clear(self):
-        self.extra = {'request_id': ''}
+        self.logger.request_id = ''
 
     def process(self, msg, kwargs):
-        if self.extra['request_id']:
-            return '[id:%s] %s' % (self.extra['request_id'], msg), kwargs
+        self.extra['global_request_id'] = self.static_global_request_id
 
+        if 'extra' in kwargs.keys():
+            kwargs['extra'].update(self.extra)
+        else:
+            kwargs['extra'] = self.extra
+
+        if hasattr(self.logger, 'request_id') and self.logger.request_id:
+            self.extra['request_id'] = self.logger.request_id
+            return '[id:%s] %s' % (self.logger.request_id, msg), kwargs
         return msg, kwargs
+
+    def set_global_request_id(self, global_id):
+        """
+
+        :param global_id:
+        :return:
+        """
+        self.extra['global_request_id'] = global_id
 
 
 class Logger():
